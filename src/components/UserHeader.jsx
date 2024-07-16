5c2a65046db1a8ae8ce644c25987f81f342ab931
shipleaf <162288294+shipleaf@users.noreply.github.com>
2024-07-16 16:34:27 +0900

    test

---
+++ src/components/UserHeader.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserHeader extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default UserHeader
