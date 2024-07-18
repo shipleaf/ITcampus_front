55288630416c22532c3a0598ac3c343aff5f98a2
unknown <jji0258@gmail.com>
2024-07-19 03:44:14 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import AppRouter from './Router';
+import Top from './components/post/Top';
+import Post from './components/post/Post';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div>
-      <AppRouter setIsLoggedIn={setIsLoggedIn} />
+      <Top />
+      <Post />
+      <AppRouter />
+      
+      <AppRouter  />
     </div>
   );
 }
